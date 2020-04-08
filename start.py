ca3dc292c012e136cbcd0ecb66b882f5250464f1
gimpy52 <abe.marmer@gmail.com>
2020-04-08 10:53:07 -0400

    lame example copied in

--- start.py
+++ start.py
@@ -1,5 +1,17 @@
 import ssl
+import socket
 
 """
 This is where things will go eventually
 """
+hostname = "www.google.com"
+#cert = CertInfo(host=hostname, port=443)
+#info = cert.cipher()
+
+context = ssl.create_default_context()
+
+conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=hostname)
+conn.connect(('google.com', 443))
+
+cert = conn.getpeercert()
+print(cert)
